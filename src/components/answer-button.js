dd2e9c5e4c073c8b5a0c1b05c83e9f472eba8ee8
aelindwatson <aelindwatson@gmail.com>
2019-01-20 12:04:22 -0500

    Component Revision

---
+++ src/components/answer-button.js
@@ -0,0 +1,12 @@
+import React { Component }, from 'react';
+import './answer-button.css';
+
+class AnswerButton extends Component {
+  render() {
+    return (
+      <div>AnswerButton</div>
+    );
+  }
+}
+
+export default AnswerButton;
